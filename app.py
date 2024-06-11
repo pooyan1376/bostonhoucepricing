a5095689f5e745d116b956b705f4867a2377616c
pooyan1376 <pooyan.hejazimahabadi@bvtech.com>
2024-06-11 17:19:03 +0200

    I have modified the the app.py file

--- app.py
+++ app.py
@@ -12,13 +12,13 @@ scaler = pickle.load(open("scaling.pkl", "rb"))
 def home():
     return render_template("home.html")
 
-@app.route("/predict_api", methods = ["POST"])
-
-def predict_api():
-    data = request.json["data"]
-    new_data = scaler.transform(np.array(list(data.values())).reshape(1, -1))
-    output = regmodel.predict(new_data)
-    return jsonify(output[0])
+@app.route('/predict',methods=['POST'])
+def predict():
+    data=[float(x) for x in request.form.values()]
+    final_input=scaler.transform(np.array(data).reshape(1,-1))
+    print(final_input)
+    output=regmodel.predict(final_input)[0]
+    return render_template("home.html",prediction_text="The House price prediction is {}".format(output))
 
 if __name__ == "__main__":
     app.run(debug=True)
